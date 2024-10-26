bfdffba6370e21732209a7054d60b1c09bad8fb6
vnebelung <vnebelung@prodisfuzz.net>
2024-10-26 23:57:18 +0200

    Updated copyright notice.

--- src/main/java/internal/parameters/IntegerParameter.java
+++ src/main/java/internal/parameters/IntegerParameter.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of ProDisFuzz, modified on 20.10.24, 15:58.
+ * This file is part of ProDisFuzz, modified on 26.10.24, 23:55.
  * Copyright (c) 2013-2024 Volker Nebelung <vnebelung@prodisfuzz.net>
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
